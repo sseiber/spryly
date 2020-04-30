adc9d2f4f6f8b639470a1ccb1a842d1b1ae86595
Scott Seiber <scott.seiber@outlook.com>
2020-04-29 18:26:47 -0700

    * Export CustomLogger type * Change ComposeOptions to optional

--- src/composition.ts
+++ src/composition.ts
@@ -1,15 +1,19 @@
 // tslint:disable-next-line:ordered-imports
 import { Server, ServerOptions } from '@hapi/hapi';
-import { join, resolve } from 'path';
+import { join as pathJoin, resolve as pathResolve } from 'path';
 import { process as process_registration, ManifestPlugin } from './registration';
 import { process as process_services } from './services';
 
 export type ManifestService = string;
 export type CustomLogger = (tags: string[], message: string) => void;
 
+// production logging hapi-pino default output   ||  handled by logCompose = true
+// dev logging hapi-pino                         ||  handled by logCompose = { prettyPrint options }
+// production logging logger function            ||  handled by logging function (client handles PROD/DEV)
+// dev logging logger function                   ||  handled by logging function (client handles PROD/DEV)
 export interface ComposeOptions {
     relativeTo?: string;
-    logCompose: boolean;
+    logCompose?: any;
     logger?: CustomLogger;
 }
 
@@ -23,36 +27,40 @@ export interface ComposeManifest {
     plugins: ManifestPlugin[];
 }
 
-export async function compose(manifest: ComposeManifest, options: ComposeOptions): Promise<Server> {
+export async function compose(manifest: ComposeManifest, options?: ComposeOptions): Promise<Server> {
     const server = new Server(manifest.server);
 
-    if (options.logCompose) {
-        if (typeof options.logger === 'function') {
-            options.logger = options.logger;
+    const composeOptions: any = {
+        relativeTo: pathResolve(__dirname, '../../../dist'),
+        logCompose: false,
+        logger: (tags: string[], message: string) => {
+            server.log(tags, message);
         }
-        else {
-            await server.register({
-                plugin: await import('hapi-pino'),
-                options: {
-                    prettyPrint: {
+    };
+
+    if (typeof options?.logger === 'function') {
+        composeOptions.logger = options.logger;
+    }
+    else {
+        await server.register({
+            plugin: await import('hapi-pino'),
+            options: {
+                prettyPrint: options?.logCompose === true
+                    ? {
                         colorize: true,
                         messageFormat: '[{tags}] {data}',
                         translateTime: 'SYS:yyyy-mm-dd"T"HH:MM:sso',
-                        ignore: 'pid,hostname,tags,data'
+                        ignore: 'pid,hostname,tags,data,req,res,responseTime'
                     }
-                }
-            });
-
-            options.logger = (tags: string[], message: string) => {
-                server.log(tags, message);
-            };
-        }
-    }
-    else {
-        // @ts-ignore
-        options.logger = (tags: string[], message: string) => { /**/ };
+                    : options?.logCompose || false
+            }
+        });
     }
 
+    return composeServer(server, manifest, composeOptions);
+}
+
+async function composeServer(server: Server, manifest: ComposeManifest, options: ComposeOptions) {
     options.logger(['spryly', 'info'], 'Composing server...');
 
     const statics = new Map<string, any>([['$server', server]]);
@@ -100,7 +108,7 @@ export function isClass(func: any): boolean {
 
 function requireRelative(id: string, options: ComposeOptions) {
     if (id.startsWith('./') && options.relativeTo) {
-        id = join(options.relativeTo, id);
+        id = pathJoin(options.relativeTo, id);
     }
 
     let instance = null;
@@ -110,7 +118,7 @@ function requireRelative(id: string, options: ComposeOptions) {
     }
     catch (e) {
         // This is to support multi-project local development
-        instance = require(resolve(process.cwd(), 'node_modules', id));
+        instance = require(pathResolve(process.cwd(), 'node_modules', id));
     }
 
     return instance;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // Export items
 
 export { HapiPlugin, PluginRegistration, AsyncPluginFunction } from './registration';
-export { ComposeOptions, ComposeManifest, compose } from './composition';
+export { CustomLogger, ComposeOptions, ComposeManifest, compose } from './composition';
 export { route, RoutePlugin, RouteOptions } from './routing';
 export { websocket, WebSocketRequest, WebSocketHandlerConfiguration } from './websockets';
 export { service, inject, ServiceContext } from './injection';
